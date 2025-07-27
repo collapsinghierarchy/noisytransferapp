0c75b8d9a54aac1b79df03dcaf849c27b14dac17
collapsinghierarchy <58694234+collapsinghierarchy@users.noreply.github.com>
2025-07-27 15:25:41 +0200

    Update IndexPage.vue

--- web/src/pages/IndexPage.vue
+++ web/src/pages/IndexPage.vue
@@ -1,5 +1,8 @@
 <template>
   <q-page class="flex items-center justify-center q-pa-md">
+    <q-banner dense class="bg-primary text-white q-mb-lg">
+      NoisyTransfer lets you share (end-to-end encrypted) files using a single link or QR-Code. Just scan or share with anyone or any of your devices and confirm the code. No Cloud, no Passwords, no same Network requirement.
+    </q-banner>
     <div class="column items-center q-gutter-y-xl">
       <q-btn
         to="/send"
